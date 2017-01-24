3dbf1354e132d0d8b41ff7b4ac91e957154d8696
marcogbarcellos <marcogbarcellos@gmail.com>
2017-01-24 10:33:33 -0500

    improving code

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 This library is responsible for all the services related to AWS S3.
 
+## Methods
+
+### Bots
+
+- `upload(bucket, req, cb)` (return: function) - Uploads to S3
+
+
 ## Usage
 
 ### Upload to S3
@@ -9,15 +16,34 @@ This library is responsible for all the services related to AWS S3.
 ```js
 const nanopayS3 = require('nanopay-s3');
 
-const s3 = new nanopayS3(process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY, process.env.NODE_ENV);
+/**
+ * Upload file to a local directory temporary(AWS S3 doesn't allow streams), then upload to AWS S3
+ * and then finally remove the temp file
+ *
+ * @param {string} accessKey - AWS Access Key
+ * @param {string} secretKey - AWS Secret Key
+ * @private
+ */
+const s3 = new nanopayS3(process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY);
 
-const S3_BUCKET = 'nanopay-img';
+const bucket = 'bucket-name';
+const bucketPath = 'path/inside/the/bucket';
 
-s3.upload(S3_BUCKET, req, function (error, response) {
+/**
+ * Upload file to a local directory temporary(AWS S3 doesn't allow streams), then upload to AWS S3
+ * and then finally remove the temp file
+ * @param {string} bucket - the S3 bucket 
+ * @param {string} bucketPath - the S3 Bucket Path 
+ * @param {object} req - the HTTP request containg the file(Stream)
+ * @returns {function} cb - the callback
+ * @private
+ */
+s3.upload(bucket, bucketPath, req, function (error, response) {
   if (error) {
     cb(error);
   }
   
   cb(false, response);
 });
+
 ```
--- index.js
+++ index.js
@@ -1,36 +1,33 @@
 'use strict'
 
 const AWS = require('aws-sdk');
-const fs = require('fs');
 const formidable = require('formidable');
 
 /**
  * @param {string} accessKey - AWS Access Key
  * @param {string} secretKey - AWS Secret Key
- * @param {string} environment - Environment String(Used to force uploading a file on the 
- *                                                  same environment that's being used)
  * @constructor
  */
-function Client(accessKey, secretKey, environment) {
+function Client(accessKey, secretKey) {
     if (!accessKey || !secretKey || !environment) {
         throw new Error('You forgot a required parameter');
     }
     AWS.config.update({ accessKeyId: accessKey, secretAccessKey: secretKey });
     
     this.s3 = new AWS.S3();
-    this.environment = environment;
 }
 
 /**
  * Upload file to a local directory temporary(AWS S3 doesn't allow streams), then upload to AWS S3
  * and then finally remove the temp file
  *
- * @param {object} bucket - the S3 Bucket Name
- * @param {object} req - the request containg the file(Stream)
+ * @param {string} bucket - the S3 bucket 
+ * @param {string} bucketPath - the S3 Bucket Path
+ * @param {object} req - the HTTP request containg the file(Stream)
  * @returns {function}
  * @private
  */
-Client.prototype.upload = function (bucket, req, cb) {
+Client.prototype.upload = function (bucket, bucketPath, req, cb) {
 
   const self = this;
   
@@ -48,7 +45,7 @@ Client.prototype.upload = function (bucket, req, cb) {
 
       self.s3.upload({
         "Bucket": bucket,
-        "Key": self.environment+'/'+uniqueName,
+        "Key": bucketPath+'/'+uniqueName,
         "Body": data
       }, function (error, response) {
         if (error) {
