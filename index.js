94e6020c89baf8da17ec1c952a42da91b1372e5f
marcogbarcellos <marcogbarcellos@gmail.com>
2017-02-02 19:28:03 -0500

    fixing major bug on data listener

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function Client(accessKey, secretKey) {
         throw new Error('You forgot a required parameter');
     }
     AWS.config.update({ accessKeyId: accessKey, secretAccessKey: secretKey });
-    
+
     this.s3 = new AWS.S3();
 
     // Promisifying all prototypes methods with suffix "Sync"...
@@ -25,7 +25,7 @@ function Client(accessKey, secretKey) {
  * Upload file to a local directory temporary(AWS S3 doesn't allow streams), then upload to AWS S3
  * and then finally remove the temp file
  *
- * @param {string} bucket - the S3 bucket 
+ * @param {string} bucket - the S3 bucket
  * @param {string} bucketPath - the S3 Bucket Path
  * @param {string} acl - the ACL the defines the permissions for the file, see more: http://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html
  * @param {object} req - the HTTP request containg the file(Stream)
@@ -35,38 +35,46 @@ function Client(accessKey, secretKey) {
 Client.prototype.upload = function (bucket, bucketPath, acl, req, cb) {
 
   const self = this;
-  
+
   const form = new formidable.IncomingForm();
+
+  let filename;
+
+  let totalData=Buffer.alloc(0);
 
   form.on('error', function (err) {
     cb(err);
   });
 
   form.onPart = function(part) {
-    
+    filename = part.filename;
     part.addListener('data', function (data) {
-      
-      const uniqueName = (new Date()).getTime() + part.filename;
-
-      self.s3.upload({
-        "Bucket": bucket,
-        "Key": bucketPath+'/'+uniqueName,
-        "Body": data,
-        "ACL": acl
-      }, function (error, response) {
-        if (error) {
-          return cb(error);
-        }
-        
-        cb(false, response);
-      }); 
+      totalData = Buffer.concat([totalData,data],totalData.length+data.length);
     });
-    
+
   }
+
+  form.on('end', function () {
+    const uniqueName = (new Date()).getTime() + filename;
+
+    self.s3.upload({
+      "Bucket": bucket,
+      "Key": bucketPath+'/'+uniqueName,
+      "Body": totalData,
+      "ACL": acl
+    }, function (error, response) {
+      if (error) {
+        return cb(error);
+      }
+
+      cb(false, response);
+    });
+
+  });
 
   // parse the incoming request containing the form data
   form.parse(req);
 
-}; 
+};
 
 module.exports = Client;
